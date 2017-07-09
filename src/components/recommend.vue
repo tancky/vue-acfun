91ece37848e01f4f82ad5f51b70eb2b30ca87aba
tancky <546349822@qq.com>
2017-07-09 18:13:16 +0800

    推荐页面接口数据渲染

--- src/components/recommend.vue
+++ src/components/recommend.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="wrap">
     <a-swipe></a-swipe>
+    <div class="monkey-topLine">
+      <img src="http://imgs.aixifan.com/cms/2017_03_03/1488528971079.gif?imageView2/1/w/100/h/100" alt="" class="fl">
+      <h4>猴山头条</h4>
+    </div>
+    <div class="top-content">
+      <div class="top-item col-xs-6" v-for="item in recommendList" key="item.aid">
+        <img :src="item.pic" alt="" class="show-img">
+        <div class="top-info">
+          <span class="top-tag">【{{item.typename}}】</span>
+          <span class="top-title">{{item.title}}</span>
+          <div class="top-user">
+            <span class="top-author">{{item.author}}</span>
+            <i class="fa fa-eye"></i>
+            <span class="watch-num">{{item.play}}</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,12 +31,24 @@
 
           }
       },
-    components: {
+      components: {
           aSwipe
-    }
+      },
+      computed: {
+          recommendList() {
+              return this.$store.state.recommendList;
+          }
+      },
+      created() {
+          this.axios.get('/api/recommend-list')
+            .then(res => {
+                console.log(res);
+              this.$store.state.recommendList = res.data.list;
+            })
+      }
   }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+  @import '../assets/css/recommend.less';
 </style>
