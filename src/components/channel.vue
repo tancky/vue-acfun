d7bbab455c8d8217c70c3297eaeaa9a3ac7d2602
tancky <546349822@qq.com>
2017-07-10 17:12:19 +0800

    新增频道页面组件及样式优化

--- src/components/channel.vue
+++ src/components/channel.vue
@@ -1,6 +1,132 @@
 <template>
-  <div class="wrapper" style="text-align: center; margin-top: 1rem; color: #D2D2D2">
-    频道页面
+  <div class="wrap">
+    <div class="channel-nav">
+      <div class="clearfix">
+        <h4>番剧</h4>
+        <span>更多<i class="fa fa-angle-right"></i></span>
+      </div>
+      <ul class="play-nav">
+        <li>新番连载</li>
+        <li>旧番补档</li>
+        <li>过场动画</li>
+        <li>动画资讯</li>
+      </ul>
+    </div>
+    <div class="channel-nav">
+      <div class="clearfix">
+        <h4>动画</h4>
+        <span>更多<i class="fa fa-angle-right"></i></span>
+      </div>
+      <ul class="animate-nav">
+        <li>MAD·AMV</li>
+        <li>MMD·3D</li>
+        <li>2.5次元</li>
+      </ul>
+    </div>
+    <div class="channel-nav">
+      <div class="clearfix">
+        <h4>娱乐</h4>
+        <span>更多<i class="fa fa-angle-right"></i></span>
+      </div>
+      <ul class="disport-nav">
+        <li>生活娱乐</li>
+        <li>鬼畜调教</li>
+        <li>萌宠</li>
+        <li>美食</li>
+        <li>综艺</li>
+      </ul>
+    </div>
+    <div class="channel-nav">
+      <div class="clearfix">
+        <h4>游戏</h4>
+        <span>更多<i class="fa fa-angle-right"></i></span>
+      </div>
+      <ul class="game-nav">
+        <li>游戏集锦</li>
+        <li>电子竞技</li>
+        <li>主机单机</li>
+        <li>英雄联盟</li>
+        <li>守望先锋</li>
+        <li>桌游卡牌</li>
+        <li>Mugen</li>
+      </ul>
+    </div>
+    <div class="channel-nav">
+      <div class="clearfix">
+        <h4>音乐</h4>
+        <span>更多<i class="fa fa-angle-right"></i></span>
+      </div>
+      <ul class="music-nav">
+        <li>原创·翻唱</li>
+        <li>演奏</li>
+        <li>Vocaloid</li>
+        <li>日系音乐</li>
+        <li>综合音乐</li>
+        <li>演唱会</li>
+      </ul>
+    </div>
+    <div class="channel-nav">
+      <div class="clearfix">
+        <h4>舞蹈</h4>
+        <span>更多<i class="fa fa-angle-right"></i></span>
+      </div>
+      <ul class="dance-nav">
+        <li>宅舞</li>
+        <li>综合舞蹈</li>
+        <li>爱豆</li>
+        <li>手作</li>
+        <li>造型</li>
+      </ul>
+    </div>
+    <div class="channel-nav">
+      <div class="clearfix">
+        <h4>科技</h4>
+        <span>更多<i class="fa fa-angle-right"></i></span>
+      </div>
+      <ul class="science-nav">
+        <li>科学技术</li>
+        <li>教程</li>
+        <li>数码</li>
+        <li>汽车</li>
+        <li>广告</li>
+      </ul>
+    </div>
+    <div class="channel-nav">
+      <div class="clearfix">
+        <h4>体育</h4>
+        <span>更多<i class="fa fa-angle-right"></i></span>
+      </div>
+      <ul class="sport-nav">
+        <li>综合体育</li>
+        <li>足球</li>
+        <li>篮球</li>
+        <li>搏击</li>
+        <li>11区体育</li>
+        <li>惊奇体育</li>
+      </ul>
+    </div>
+    <div class="channel-nav">
+      <div class="clearfix">
+        <h4>鱼塘</h4>
+        <span>更多<i class="fa fa-angle-right"></i></span>
+      </div>
+      <ul class="pound-nav">
+        <li>历史</li>
+      </ul>
+    </div>
+    <div class="channel-nav">
+      <div class="clearfix">
+        <h4>文章</h4>
+        <span>更多<i class="fa fa-angle-right"></i></span>
+      </div>
+      <ul class="article-nav">
+        <li>工作情感</li>
+        <li>动漫文化</li>
+        <li>漫画小说</li>
+        <li>综合</li>
+        <li>游戏</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -14,6 +140,6 @@
   }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+  @import '../assets/css/channel.less';
 </style>
